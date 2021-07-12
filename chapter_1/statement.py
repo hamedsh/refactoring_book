3b013c78e7d0176d3e5f6f1c021b3439eb69770f
hamedSh <hamed.sheykhlu@gmail.com>
2021-07-12 15:29:12 +0430

    refactoring: Change Function Declaration
    chapter 1: Refactoring: A First Example -> Decomposing the statement Function -> Removing the play Variable

--- chapter_1/statement.py
+++ chapter_1/statement.py
@@ -11,20 +11,19 @@ class Statement:
     def format_currency(value: float) -> str:
         return f'${value:,.2f}'
 
-    @staticmethod
-    def amount_for(a_performance: Dict[str, Any], play: Dict[str, Any]) -> int:
+    def amount_for(self, a_performance: Dict[str, Any]) -> int:
         result: int = 0
-        if play["type"] == "tragedy":
+        if self.play_for(a_performance)["type"] == "tragedy":
             result = 40000
             if a_performance["audience"] > 30:
                 result += 1000 * (a_performance["audience"] - 30)
-        elif play["type"] == "comedy":
+        elif self.play_for(a_performance)["type"] == "comedy":
             result = 30000
             if a_performance['audience'] > 20:
                 result += 10000 + 500 * (a_performance["audience"] - 20)
             result += 300 * a_performance["audience"]
         else:
-            raise Exception(f'Unknown type: {play["type"]}')
+            raise Exception(f'Unknown type: {self.play_for(a_performance)["type"]}')
         return result
 
     def play_for(self, a_performance: Dict[str, Any]) -> Dict[str, Any]:
@@ -37,7 +36,7 @@ class Statement:
         result = f'Statement for {invoice["customer"]}\n'
 
         for perf in invoice["performances"]:
-            this_amount = Statement.amount_for(perf, self.play_for(perf))
+            this_amount = self.amount_for(perf)
             # add volume credits
             volume_credits += max(perf['audience'] - 30, 0)
             # add extra credits for every ten comedy attendees
